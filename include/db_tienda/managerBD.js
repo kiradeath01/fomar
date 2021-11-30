3fbb8cfa8d2c75257a6624b052044b3473e1c28d
Your Name <chesman021@gmail.com>
2021-11-29 21:39:32 -0600

    actualizacion tienda.php

--- include/db_tienda/managerBD.js
+++ include/db_tienda/managerBD.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
             var i = 0;
             var img=resultado.descripcionesimg;
             var descripcion=resultado.descripciones;
-            console.log(resultado);
             $("#nombre_producto").empty();
             $("#precio").empty();
             $("#stock").empty();
@@ -18,7 +17,7 @@ $(document).ready(function(){
             $("#precio").append('<span class="display-4">$ '+resultado.precio+'</span>');
             $("#stock").append('Stock disponible <b>'+resultado.stock+'</b>');
             $("#container-zoom").empty();
-            $("#container-zoom").append('<img id="zoom_03" src="'+img[0].url_imagen+'" class="img-fluid" alt="..." ></img>');
+            $("#container-zoom").append('<img id="zoom_03" src="'+img[0].url_imagen+'" data-zoom-image="'+img[0].url_imagen+'" class="img-fluid" alt="..." ></img>');
             
             //*
             $("#gal1").empty();
@@ -48,15 +47,10 @@ $(document).ready(function(){
         .then(response => {
             var resultado = JSON.parse(JSON.stringify(response.data)); 
             var i = 0;
-            var img=resultado.descripcionesimg;
-            $.each(resultado, function(i, obj){   
-                i++;
-            }); //termina foreach  
-            /*
             $("#galeria_productos").empty();
-            if(!$.isEmptyObject(img)){
-                $.each(img, function(i, obj){   
-                    $("#galeria_productos").append('<a href="#" data-image="'+obj.url_imagen+'" data-zoom-image="'+obj.url_imagen+'"><img id="img_01" class="img-thumbnail mb-2" src="'+obj.url_imagen+'"/></a>');
+            if(!$.isEmptyObject(resultado)){
+                $.each(resultado, function(i, obj){   
+                    $("#galeria_productos").append('<div class="d-flex justify-content-center p-1"><div class="col p-0 m-0 text-center"><img class="rounded img-fluid " src="'+obj.urlimg+'" alt="Third slide"><p style="font-size:11px;"><strong>'+obj.nombre_producto+'</strong></p></div></div>');
                     i++;
                 }); //termina foreach  
             }//termina if de emptyobject
@@ -65,18 +59,15 @@ $(document).ready(function(){
         .catch(error => console.error('Ups, ha ocurrido un error', error));  
 });
 
-$('#zoom_03').ezPlus({
-    gallery: 'gal1', 
-    cursor: 'pointer', 
-    galleryActiveClass: 'active',
-    imageCrossfade: false,
-  //  zoomType: 'lens',
-   //containLensZoom: true,
-    loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif'
-  });
 
-  $("#zoom_03").bind("click",function (e) {
+$('#zoom_03').ezPlus({
+    gallery: 'gallery_01', cursor: 'pointer', galleryActiveClass: 'active',
+    imageCrossfade: true, loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif'
+});
+
+//pass the images to Fancybox
+$('#zoom_03').bind('click', function (e) {
     var ez = $('#zoom_03').data('ezPlus');
     $.fancyboxPlus(ez.getGalleryList());
     return false;
-  });
+});
--- tienda.php
+++ tienda.php
@@ -64,47 +64,7 @@
                       <div class="card-body">
                    
                         <div class="autoplay " id="galeria_productos">
-                          <div class="d-flex justify-content-center p-1">
-                            <div class="col p-0 m-0 text-center">
-                               <img class="rounded img-fluid " src="img/small/image2.jpg" alt="Third slide"> 
-                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
-                            </div>  
-                          </div>
-
-                          <div class="d-flex justify-content-center p-1">
-                            <div class="col p-0 m-0 text-center">
-                               <img class="rounded img-fluid " src="img/small/image4.jpg" alt="Third slide"> 
-                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
-                            </div>  
-                          </div>
-
-                          <div class="d-flex justify-content-center p-1">
-                            <div class="col p-0 m-0 text-center">
-                               <img class="rounded img-fluid " src="img/small/image3.jpg" alt="Third slide"> 
-                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
-                            </div>  
-                          </div>
-
-                          <div class="d-flex justify-content-center p-1">
-                            <div class="col p-0 m-0 text-center">
-                               <img class="rounded img-fluid " src="img/small/image1.jpg" alt="Third slide"> 
-                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
-                            </div>  
-                          </div>
-
-                          <div class="d-flex justify-content-center p-1">
-                            <div class="col p-0 m-0 text-center">
-                               <img class="rounded img-fluid " src="img/small/image4.jpg" alt="Third slide"> 
-                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
-                            </div>  
-                          </div>
-
-                          <div class="d-flex justify-content-center p-1">
-                            <div class="col p-0 m-0 text-center">
-                               <img class="rounded img-fluid " src="img/small/image3.jpg" alt="Third slide"> 
-                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
-                            </div>  
-                          </div>
+                          
 
                         </div>
                                            
@@ -194,7 +154,6 @@
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/igorlino/elevatezoom-plus@1.2.3/src/jquery.ez-plus.js"></script>
     <script src="js/menu.js"></script>
